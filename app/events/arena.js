0b3c14f7c0195e456b99b7ea7d9c451c1ffe7250
Rodrigo Otavio van den Berg Maia <rodrigo@devlandia.net>
2020-04-24 11:54:05 -0300

    Split into functions

--- app/events/arena.js
+++ app/events/arena.js
@@ -7,50 +7,77 @@ Array.prototype.sample = function(){
 
 const players = {}
 
+function buildTeam(userId){
+  const positions = [ 'Goleiro', 'Zagueiro', 'Zagueiro', 'Zagueiro', 'Zagueiro',
+    'Volante', 'Volante', 'Volante', 'Atacante', 'Atacante', 'Atacante' ]
+
+  // Initialize player team
+  players[userId] = {
+    team      : [],
+    movements : []
+  }
+
+  // build the team for informed userId
+  for(var i = 0; i < 11; i++){
+    players[userId].team.push(Player.build({
+      name      : `Jogador ${i + 1}`,
+      position  : positions[i],
+      ownBall   : false,
+      movements : []
+    }))
+
+    // TODO: Just for tests
+    i += 2
+  }
+}
+
+/**
+ * Set ownBall of all players from all teams as false.
+ */
+function clearOwnBall(){
+  for (var [key, value] of Object.entries(players)) {
+    for(var [k, v] of Object.entries(value.team)){
+      v.ownBall = false
+    }
+  }
+}
+
+/**
+ * Gives the ball to a team.
+ * Clear all player movements.
+ */
+function ownBall(userId){
+  players[userId].team[1].ownBall = true
+
+  for (var [player, values] of Object.entries(players)) {
+    player.movements = []
+  }
+}
+
 module.exports = (io, socket) => {
   /**
    * Creates an array of players to informed userId
    * When players has 2 entries, get a random player and gives tha ball to first defender of team.
    */
   socket.on('init game', (userId) => {
-
     console.log(`Game started as ${userId}`)
 
-    const positions = [ 'Goleiro', 'Zagueiro', 'Zagueiro', 'Zagueiro', 'Zagueiro',
-      'Volante', 'Volante', 'Volante', 'Atacante', 'Atacante', 'Atacante' ]
-
-    // Initialize player team
-    players[userId] = {
-      team: []
-    }
     let currentPlayer
 
-    // build the team for informed userId
-    for(var i = 0; i < 11; i++){
-      players[userId].team.push(Player.build({
-        name      : `Jogador ${i + 1}`,
-        position  : positions[i],
-        ownBall   : false,
-        movements : []
-      }))
+    buildTeam(userId) // creates the team for each user
+    clearOwnBall()    // set all players as non owner
 
-      // TODO: Just for tests
-      i += 2
-    }
-
-    // Clear ownBall of all players and set it again.
-    for (var [key, value] of Object.entries(players)) {
-      for(var [k, v] of Object.entries(value.team)){
-        v.ownBall = false
-      }
-    }
+    // pick one player to start the match.
     keys = Object.keys(players)
     if(keys.length == 2){
       currentPlayer = keys.sample()
 
-      players[currentPlayer].team[1].ownBall = true
+      ownBall(currentPlayer)
     }
 
-    io.emit('refresh deck', currentPlayer, JSON.stringify(players))
+    io.emit('refresh deck', userId, JSON.stringify(players))
+  })
+
+  socket.on('movement', function(){
   })
 }
